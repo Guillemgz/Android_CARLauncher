24595d2bdbb9d3e368c614e2cc615f0236a3e852
guillemgz <guillermoomg@gmail.com>
2021-10-31 20:29:32 +0100

    1. java clases done

---
+++ app/src/main/java/com/example/android_carlauncher/apps_info.java
@@ -0,0 +1,9 @@
+package com.example.android_carlauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class apps_info {
+    CharSequence label;
+    CharSequence packageName;
+    Drawable icon;
+}
